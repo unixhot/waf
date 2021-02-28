941670eca8211c5c9ae2f3772132c6f7f9ce614a
opswill <7550211+opswill@users.noreply.github.com>
2021-02-28 15:10:10 +0800

    调整策略优先级
    waf策略改为先白后黑，同时开启post过滤

--- waf/access.lua
+++ waf/access.lua
@@ -2,14 +2,14 @@ require 'init'
 
 function waf_main()
     if white_ip_check() then
+    elseif white_url_check() then
     elseif black_ip_check() then
     elseif user_agent_attack_check() then
     elseif cc_attack_check() then
     elseif cookie_attack_check() then
-    elseif white_url_check() then
     elseif url_attack_check() then
     elseif url_args_attack_check() then
-    --elseif post_attack_check() then
+    elseif post_attack_check() then
     else
         return
     end
